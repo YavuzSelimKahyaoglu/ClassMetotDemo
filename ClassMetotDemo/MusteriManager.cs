c0ef714a29df0c1911d7245667ee408cf16094b2
Yavuz Kahyaoğlu <yavuzkahyaoglu27@gmail.com>
2021-01-17 23:52:09 +0300

    17 Ocak 2021 Kamp Ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public ArrayList musteriler = new ArrayList();
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("-----EKLEME----");
+            musteriler.Add(musteri);
+            string isim = musteri.Adi + " " + musteri.Soyad;
+            Console.WriteLine("Müşteri Başarıyla Eklendi! ("+musteri.Id+":"+isim+")");
+            
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("-----SİLME-----");
+            musteriler.Remove(musteri);
+            string isim = musteri.Adi + " "+musteri.Soyad;
+            Console.WriteLine("Müşteri Başarıyla Silindi! ("+musteri.Id+":"+isim+")");
+            
+        }
+
+        public void MusteriListele()
+        {
+            
+            Console.WriteLine("---LİSTELEME---");
+            if (musteriler.Count==0)
+            {
+                Console.WriteLine("Henüz Bir Müşteri Bulunmamaktadır!");
+            }
+            else
+            {
+                foreach (Musteri musteri1 in musteriler)
+                {
+                    string tamlama = musteri1.Id+":"+musteri1.Adi+" "+musteri1.Soyad;
+                    Console.WriteLine(tamlama);
+                }
+            }           
+            
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,30 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0;
+            musteri1.Adi = "Yavuz";
+            musteri1.Soyad = "Kahyaoğlu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 1;
+            musteri2.Adi = "Selim";
+            musteri2.Soyad = "Kahyaoğlu";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 2;
+            musteri3.Adi = "Yavuz Selim";
+            musteri3.Soyad = "Kahyaoğlu";
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
+            musteriManager.MusteriSil(musteri2);
+
+            musteriManager.MusteriListele();
         }
     }
 }
